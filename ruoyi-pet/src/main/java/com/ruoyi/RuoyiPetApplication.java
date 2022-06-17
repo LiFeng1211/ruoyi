37bc874b48b1679a7a1f2175eba3faf73b9b79c1
LiFeng1211 <107463140+LiFeng1211@users.noreply.github.com>
2022-06-17 15:14:44 +0800

    Add files via upload

---
+++ ruoyi-pet/src/main/java/com/ruoyi/RuoyiPetApplication.java
@@ -0,0 +1,13 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoyiPetApplication.class, args);
+    }
+
+}
---
+++ ruoyi-pet/src/test/java/com/ruoyi/RuoyiPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruoyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuoyiPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
